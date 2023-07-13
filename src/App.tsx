cf2277410b077b474a8d9f8ce09529b053538d5a
Will Kelly <willkelly9617@gmail.com>
2023-07-13 13:58:31 -0500

    Initial commit of gql viewer

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
+// @ts-ignore - types don't exist
+import HasuraGraphiQL from "@hasura/public-graphiql";
+import {getPresets} from "./utils";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const {
+    defaultHeaders,
+    defaultQuery,
+    defaultUrl,
+    defaultVariables,
+    queryFile,
+  } = getPresets();
+  const [endpoint, setEndpoint] = useState(defaultUrl);
+
+  if (endpoint.length) {
+    return (
+      <>
+        <div className="graphiql-holder">
+          <HasuraGraphiQL
+            url={endpoint}
+            defaultHeaders={defaultHeaders}
+            defaultQuery={defaultQuery}
+            defaultVariables={defaultVariables}
+            isCloud={false}
+            customToolbar={
+              <button
+                onClick={() => setEndpoint("")}
+                className="change-endpoint-button"
+              >
+                Change Endpoint
+              </button>
+            }
+          />
+        </div>
+      </>
+    );
+  } else return null;
 }
 
 export default App;
